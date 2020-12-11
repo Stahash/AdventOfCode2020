079f4a5e6cc83cc61b615f2fb7fa0fa2e2f7b3e5
Stahash <stakhanova.anastasia@gmail.com>
2020-12-11 19:13:57 +0100

    Day 11 Part 2

--- src/day_11.py
+++ src/day_11.py
@@ -1,6 +1,6 @@
 from sys import argv
 from os import chdir
-from numpy import zeros, array_equal, count_nonzero
+from numpy import zeros, array_equal, count_nonzero, diagonal
 
 
 def create_input_matrix(data):
@@ -43,33 +43,14 @@ def neighbours_exist(current_row, current_column, row_limit, column_limit):
     return coordinates_to_look
 
 
-def check_occupied_adjacent(list_coordinates, all_data):
-    change_to_empty_flag = False
+def count_occupied(list_coordinates, all_data):
 
-    count_occupied = 0
+    number_occupied = 0
     for coordinate_pair in list_coordinates:
         if all_data[coordinate_pair[0], coordinate_pair[1]] == '#':
-            count_occupied += 1
+            number_occupied += 1
 
-    if count_occupied >= 4:
-        change_to_empty_flag = True
-
-    return change_to_empty_flag
-
-
-def check_empty_adjacent(list_coordinates, all_data):
-
-    change_to_occupied_flag = False
-
-    count_occupied = 0
-    for coordinate_pair in list_coordinates:
-        if all_data[coordinate_pair[0], coordinate_pair[1]] == '#':
-            count_occupied += 1
-
-    if count_occupied == 0:
-        change_to_occupied_flag = True
-
-    return change_to_occupied_flag
+    return number_occupied
 
 
 def apply_rules(data):
@@ -89,14 +70,150 @@ def apply_rules(data):
                 changed_data[row, column] = '.'
 
             elif data[row, column] == '#':
-                change_to_empty = check_occupied_adjacent(existing_neighbours, data)
-                if change_to_empty is True:
+                occupied_seats = count_occupied(existing_neighbours, data)
+                if occupied_seats >= 4:
                     changed_data[row, column] = 'L'
                 else:
                     changed_data[row, column] = '#'
+
             elif data[row, column] == 'L':
-                change_to_occupied = check_empty_adjacent(existing_neighbours, data)
-                if change_to_occupied is True:
+                occupied_seats = count_occupied(existing_neighbours, data)
+                if occupied_seats == 0:
+                    changed_data[row, column] = '#'
+                else:
+                    changed_data[row, column] = 'L'
+
+    return changed_data
+
+
+def neighbours_exist_new(current_row, current_column, row_limit, column_limit):
+
+    directions_to_look = []
+
+    if (current_row - 1) >= 0:  # upper
+        directions_to_look.append((-1, 0))
+
+    if ((current_row - 1) >= 0) and (current_column - 1 >= 0):  # upper left
+        directions_to_look.append((-1, -1))
+
+    if (current_column - 1) >= 0:  # left
+        directions_to_look.append((0, -1))
+
+    if ((current_row + 1) <= row_limit - 1) and ((current_column - 1) >= 0):  # down left
+        directions_to_look.append((1, -1))
+
+    if (current_row + 1) <= row_limit - 1:  # down
+        directions_to_look.append((1, 0))
+
+    if ((current_row + 1) <= row_limit - 1) and ((current_column + 1) <= column_limit - 1):  # down right
+        directions_to_look.append((1, 1))
+
+    if (current_column + 1) <= column_limit - 1:  # right
+        directions_to_look.append((0, 1))
+
+    if ((current_row - 1) >= 0) and (current_column + 1 <= column_limit - 1):  # upper right
+        directions_to_look.append((-1, 1))
+
+    return directions_to_look
+
+
+def go_along_direction(row, column, step_i, step_j, all_data):
+
+    seat_occupied = False
+    row_limit = all_data.shape[0]
+    column_limit = all_data.shape[1]
+
+    new_x = row + step_i
+    new_y = column + step_j
+
+    while (0 <= new_x <= row_limit - 1) and (0 <= new_y <= column_limit - 1):
+        if all_data[new_x, new_y] == '.':
+            new_x += step_i
+            new_y += step_j
+        else:
+            if all_data[new_x, new_y] == '#':
+                seat_occupied = True
+            else:
+                seat_occupied = False
+            return seat_occupied
+
+    return seat_occupied
+
+
+def count_occupied_new(list_coordinates, all_data, current_row, current_column):
+
+    number_occupied = 0
+    if (-1, 0) in list_coordinates:  # look to upper direction
+        is_occupied = go_along_direction(current_row, current_column, -1, 0, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    if (-1, -1) in list_coordinates:  # look to upper left
+        is_occupied = go_along_direction(current_row, current_column, -1, -1, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    if (0, -1) in list_coordinates:  # look to left direction
+        is_occupied = go_along_direction(current_row, current_column, 0, -1, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    if (1, -1) in list_coordinates:  # look to down left
+        is_occupied = go_along_direction(current_row, current_column, 1, -1, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    if (1, 0) in list_coordinates: # look down direction
+        is_occupied = go_along_direction(current_row, current_column, 1, 0, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    if (1, 1) in list_coordinates:  # look down right direction
+        is_occupied = go_along_direction(current_row, current_column, 1, 1, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    if (0, 1) in list_coordinates: # look right direction
+        is_occupied = go_along_direction(current_row, current_column, 0, 1, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    if (-1, 1) in list_coordinates: # look upper right direction
+        is_occupied = go_along_direction(current_row, current_column, -1, 1, all_data)
+        if is_occupied is True:
+            number_occupied += 1
+
+    return number_occupied
+
+
+def apply_new_rules(data):
+    # People don't just care about adjacent seats -
+    # they care about the first seat they can see in each of those eight directions!
+
+    # Also, people seem to be more tolerant than you expected:
+    # it now takes five or more visible occupied seats for an occupied seat to become empty
+    # (rather than four or more from the previous rules).
+
+    changed_data = zeros(data.shape, dtype=str, order='C')
+
+    for row in range(data.shape[0]):
+        for column in range(data.shape[1]):
+
+            existing_neighbours = neighbours_exist_new(row, column, data.shape[0], data.shape[1])
+
+            if data[row, column] == '.':
+                changed_data[row, column] = '.'
+
+            elif data[row, column] == '#':
+                occupied_seats = count_occupied_new(existing_neighbours, data, row, column)
+                if occupied_seats >=5:
+                    changed_data[row, column] = 'L'
+                else:
+                    changed_data[row, column] = '#'
+
+            elif data[row, column] == 'L':
+                occupied_seats = count_occupied_new(existing_neighbours, data, row, column)
+                if occupied_seats == 0:
                     changed_data[row, column] = '#'
                 else:
                     changed_data[row, column] = 'L'
@@ -124,8 +241,20 @@ def first_part(data):
 
 def second_part(data):
 
+    data_before_round = data
+    data_after_round = zeros(data.shape, dtype=str, order='C')
+    finished = False
 
-    return None
+    while finished is False:
+        data_after_round = apply_new_rules(data_before_round)
+        if array_equal(data_after_round, data_before_round) is True:
+            finished = True
+        else:
+            data_before_round = data_after_round
+
+    occupied_seats = count_nonzero(data_after_round == '#')
+
+    return occupied_seats
 
 
 def day_11_solution(folder_name, file_name):
@@ -138,7 +267,7 @@ def day_11_solution(folder_name, file_name):
     input_matrix = create_input_matrix(input_data)
 
     answer_1 = first_part(input_matrix)
-    answer_2 = second_part(input_data)
+    answer_2 = second_part(input_matrix)
 
     print('Your puzzle answer for first part is {0}'.format(answer_1))
     print('Your puzzle answer for second part is {0}'.format(answer_2))
