0731cc1bebe2f5433b041683bbe27e2baecfc155
Stahash <stakhanova.anastasia@gmail.com>
2020-12-20 22:04:15 +0100

    Day 15. Part 2.
    One function for both parts.
    Part 2 still takes too much time (~ 5 minutes)

--- src/day_15.py
+++ src/day_15.py
@@ -10,37 +10,43 @@ def read_input_file(path_to_file):
     return input_data
 
 
-def first_part(data):
+def first_part(data, turn_limit):
 
     turns_number = len(data)
     speaking_numbers = data.copy()
+    number_and_occurences = dict()
 
-    while turns_number < 2020:
+    for i in range(len(speaking_numbers)):
+        number_and_occurences[speaking_numbers[i]] = [i + 1]
+
+    while turns_number < turn_limit:
 
         turns_number += 1
 
         last_number = speaking_numbers[-1]
-        occurences = speaking_numbers.count(last_number)
+        occurences = len(number_and_occurences[last_number])
 
         if occurences == 1:
             speaking_numbers.append(0)
+            number_and_occurences[0] = number_and_occurences.get(0, list())
+            number_and_occurences[0].append(turns_number)
             continue
         else:
-            last_occurence = len(speaking_numbers)
-            previous_occurence = [i+1 for i, x in enumerate(speaking_numbers)
-                                  if x == last_number and i < len(speaking_numbers) - 1]
+            last_occurence = number_and_occurences[last_number][-1]
+            previous_occurence = number_and_occurences[last_number][-2]
+            number_and_occurences[last_number] = [previous_occurence, last_occurence]
 
-            speaking_numbers.append(last_occurence-previous_occurence[-1])
+            new_number = last_occurence - previous_occurence
+            speaking_numbers.append(new_number)
+
+            number_and_occurences[new_number] = number_and_occurences.get(new_number, list())
+            number_and_occurences[new_number].append(turns_number)
+
+        print('Turn {0}. Spoken number {1}'.format(turns_number, speaking_numbers[-1]))
 
     answer = speaking_numbers[-1]
 
     return answer
-
-
-def second_part(data):
-
-
-    return None
 
 
 def day_15_solution(folder_name, file_name):
@@ -49,8 +55,12 @@ def day_15_solution(folder_name, file_name):
     path_to_input = folder_name + '/' + file_name
 
     input_content = read_input_file(path_to_input)
-    answer_1 = first_part(input_content)
-    answer_2 = second_part(input_content)
+
+    max_turn = 2020
+    answer_1 = first_part(input_content, max_turn)
+
+    max_turn = 30000000
+    answer_2 = first_part(input_content, max_turn)
 
     print('Your puzzle answer for first part is {0}'.format(answer_1))
     print('Your puzzle answer for second part is {0}'.format(answer_2))
