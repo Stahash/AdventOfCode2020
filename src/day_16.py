4439fb9ce93d42064837de74fc1dac6547ddb31a
Stahash <stakhanova.anastasia@gmail.com>
2020-12-21 19:26:59 +0100

    Day 16. Part 2. Code is too long, could be shorted.

--- src/day_16.py
+++ src/day_16.py
@@ -48,8 +48,10 @@ def read_input_file(path_to_file):
 def first_part(rules, nearby_tickets):
 
     incorrect_fields = list()
+    valid_tickets = list()
 
     for ticket in nearby_tickets:
+        valid_fields = list()
         for field in ticket:
             not_valid = list()
             for rule in rules.keys():
@@ -61,16 +63,71 @@ def first_part(rules, nearby_tickets):
 
             if len(not_valid) == len(rules.keys()):
                 incorrect_fields.append(field)
+            else:
+                valid_fields.append(True)
+
+        if len(valid_fields) == len(ticket):
+            valid_tickets.append(ticket)
 
     answer = sum(incorrect_fields)
 
+    return answer, valid_tickets
+
+
+def second_part(rules, your_ticket, valid_tickets):
+
+    tickets_and_meanings = dict()
+
+    count_tickets = 0
+    for ticket in valid_tickets:
+        possible_meanings = dict()
+        for field_i in range(len(ticket)):
+            meanings = list()
+            field = ticket[field_i]
+            for rule in rules.keys():
+                rule_limits = rules[rule]
+                if (rule_limits[0][0] <= field <= rule_limits[0][1]) or (rule_limits[1][0] <= field <= rule_limits[1][1]):
+                    meanings.append(rule)
+            possible_meanings[field_i] = meanings
+        tickets_and_meanings[count_tickets] = possible_meanings
+        count_tickets += 1
+
+    tickets_number = len(tickets_and_meanings.keys())
+    fields_number = len(tickets_and_meanings[0].keys())
+
+    all_sets = dict()
+
+    for field_i in range(fields_number):
+        results_set = set(tickets_and_meanings[0][field_i])
+        for ticket_i in range(1, tickets_number):
+            new_set = set(tickets_and_meanings[ticket_i][field_i])
+            intersection = results_set.intersection(new_set)
+            results_set = intersection
+        all_sets[field_i] = results_set
+
+
+    defined = list()
+    max_field_lenght = max([len(all_sets[field]) for field in all_sets.keys()])
+    while max_field_lenght != 0:
+        defined_field_index = [field for field in all_sets.keys() if len(all_sets[field]) == 1][0]
+        defined_field_meaning = list(all_sets[defined_field_index])[0]
+        defined.append((defined_field_index, defined_field_meaning))
+        all_sets.pop(defined_field_index)
+
+        for field_i in all_sets:
+            all_sets[field_i].remove(defined_field_meaning)
+
+        if all_sets:
+            max_field_lenght = max([len(all_sets[field]) for field in all_sets.keys()])
+        else:
+            max_field_lenght = 0
+
+    answer = 1
+    for pair in defined:
+        if pair[1].startswith('departure'):
+            answer *= your_ticket[0][pair[0]]
+
     return answer
-
-
-def second_part(rules, your_ticket):
-
-
-    return None
 
 
 def day_16_solution(folder_name, file_name):
@@ -80,8 +137,8 @@ def day_16_solution(folder_name, file_name):
 
     rules, your_ticket, nearby_tickets = read_input_file(path_to_input)
 
-    answer_1 = first_part(rules, nearby_tickets)
-    answer_2 = second_part(rules, your_ticket)
+    answer_1, valid_tickets = first_part(rules, nearby_tickets)
+    answer_2 = second_part(rules, your_ticket, valid_tickets)
 
     print('Your puzzle answer for first part is {0}'.format(answer_1))
     print('Your puzzle answer for second part is {0}'.format(answer_2))
