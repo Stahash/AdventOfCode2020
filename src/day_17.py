ae9afdd1d5059613485a42809011ea4afa977f49
Stahash <stakhanova.anastasia@gmail.com>
2020-12-22 16:57:42 +0100

    Day 17. Part 2.
    Better to make boolean not string arrays.

--- src/day_17.py
+++ src/day_17.py
@@ -26,7 +26,7 @@ def change_the_cube(cube):
                 little_cube.fill('.')
                 little_cube = cube[i-1:i+2, j-1:j+2, k-1:k+2]
                 active_neighbours = count_nonzero(little_cube == '#')
-                
+
                 # limits increased by 1, because little cube is copy of the cube part and it's center can be '#'
                 if cube[i, j, k] == '#' and 3 <= active_neighbours <= 4:
                     modified_cube[i, j, k] = '#'
@@ -37,8 +37,36 @@ def change_the_cube(cube):
     return modified_cube
 
 
+def change_the_cube_4dim(cube):
+    modified_cube = zeros(cube.shape, dtype=str, order='C')
+    modified_cube.fill('.')
+
+    cube_size = cube.shape[0]
+    number_layers = cube.shape[2]
+
+    for i in range(1, cube_size-1):
+        for j in range(1, cube_size-1):
+            for k in range(1, number_layers-1):
+                for m in range(1, number_layers - 1):
+
+                    little_cube = zeros((3, 3, 3, 3), dtype=str, order='C')
+                    little_cube.fill('.')
+                    little_cube = cube[i-1:i+2, j-1:j+2, k-1:k+2, m-1:m+2]
+                    active_neighbours = count_nonzero(little_cube == '#')
+
+                    # limits increased by 1, because little cube is copy of the cube part and it's center can be '#'
+                    if cube[i, j, k, m] == '#' and 3 <= active_neighbours <= 4:
+                        modified_cube[i, j, k, m] = '#'
+
+                    if cube[i, j, k, m] == '.' and active_neighbours == 3:
+                        modified_cube[i, j, k, m] = '#'
+
+    return modified_cube
+
+
 def first_part(data, cycles):
 
+    print('First part:')
     cube_size = len(data[0]) + 4
     number_layers = 5
     cube = zeros((cube_size, cube_size, number_layers), dtype=str, order='C')
@@ -54,7 +82,7 @@ def first_part(data, cycles):
         new_cube = change_the_cube(cube)
 
         active_cubes = count_nonzero(new_cube == '#')
-        print('After {0} cycle {1} cubes are in active state'.format(cycle_i+1, active_cubes))
+        print('    After {0} cycle {1} cubes are in active state'.format(cycle_i+1, active_cubes))
 
         cube_size += 4
         number_layers += 4
@@ -66,10 +94,33 @@ def first_part(data, cycles):
     return active_cubes
 
 
-def second_part(data):
+def second_part(data, cycles):
 
+    print('Second part:')
+    cube_size = len(data[0]) + 4
+    number_layers = 5
+    cube = zeros((cube_size, cube_size, number_layers, number_layers), dtype=str, order='C')
+    cube.fill('.')
 
-    return None
+    # initialize the cube
+    for line_i in range(2, cube_size - 2):
+        cube[line_i, 2:cube_size - 2, 2, 2] = list(data[line_i - 2])
+    active_cubes = count_nonzero(cube == '#')
+
+    for cycle_i in range(cycles):
+        new_cube = change_the_cube_4dim(cube)
+
+        active_cubes = count_nonzero(new_cube == '#')
+        print('    After {0} cycle {1} cubes are in active state'.format(cycle_i + 1, active_cubes))
+
+        cube_size += 4
+        number_layers += 4
+
+        cube = zeros((cube_size, cube_size, number_layers, number_layers), dtype=str, order='C')
+        cube.fill('.')
+        cube[2:cube_size - 2, 2:cube_size - 2, 2:number_layers - 2, 2:number_layers - 2] = new_cube
+
+    return active_cubes
 
 
 def day_17_solution(folder_name, file_name):
@@ -81,7 +132,7 @@ def day_17_solution(folder_name, file_name):
 
     cycles_number = 6
     answer_1 = first_part(input_data, cycles_number)
-    answer_2 = second_part(input_data)
+    answer_2 = second_part(input_data, cycles_number)
 
     print('Your puzzle answer for first part is {0}'.format(answer_1))
     print('Your puzzle answer for second part is {0}'.format(answer_2))
